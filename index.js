897e75ccfbf34d3f642ee96bdaca813222e0878d
maithili-10 <angajalamaithiliraj@gmail.com>
2022-11-27 18:41:09 +0400

    digital clock

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const updateClock=()=>{
     h=h<10?"0"+h:h;
     m=m<10?"0"+m:m;
     s=s<10?"0"+s:s;
-    hourel.innerText=h;
+    hourel.innerText=h; 
     minel.innerText=m;
     secel.innerText=s;
     ampmel.innerText=ampm;
